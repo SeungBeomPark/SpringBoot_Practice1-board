628972816294e3ee3a6aca80a08b4b59f20011ad
psb95 <psb95@192.168.200.187>
2021-07-06 22:55:53 +0900

    2021. 7. 6 예외처리 및 메시지 알람 팝업 구현

--- Board/src/main/java/com/board/controller/BoardController.java
+++ Board/src/main/java/com/board/controller/BoardController.java
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.board.constant.Method;
 import com.board.domain.BoardDTO;
 import com.board.service.BoardService;
+import com.board.util.UiUtils;
 
 @Controller
-public class BoardController {
+public class BoardController extends UiUtils {
 	
 	@Autowired
 	private BoardService boardService;
@@ -35,19 +37,26 @@ public class BoardController {
 	}
 	
 	@PostMapping(value = "/board/register.do")
-	public String registerBoard(BoardDTO params) {
+	public String registerBoard(BoardDTO params, Model model) {
 		try {
 			boolean isRegister = boardService.registerBoard(params);
 			if(!isRegister) {
-				// 등록 실패 메시지 팝업 구현 예정
+				// 등록 실패 메시지 팝업 구현 예정(To-do)
+				return showMessageWithRedirect("게시글 등록에 실패하였습니다.",
+						"/board/list.do", Method.GET, null, model);
 			}
 		}catch(DataAccessException e) {
-			// 데이터 처리 예외 팝업 구현 예정
+			// 데이터 처리 예외 팝업 구현 예정(To-do)
+			return showMessageWithRedirect("데이터 처리 과정에서 문제가 발생하였습니다.",
+					"/board/list.do", Method.GET, null, model);
 		}catch(Exception e) {
-			// 시스템 예외 팝업 구현 예정
+			// 시스템 예외 팝업 구현 예정(To-do)
+			return showMessageWithRedirect("시스템 문제가 발생하였습니다.",
+					"/board/list.do", Method.GET, null, model);
 		}
 		
-		return "redirect:/board/list.do";
+		return showMessageWithRedirect("게시글 등록을 완료하였습니다.",
+				"/board/list.do", Method.GET, null, model);
 	}
 	
 	@GetMapping(value = "/board/list.do")
@@ -71,9 +80,39 @@ public class BoardController {
 			return "redirect:/board/list.do";
 		}
 		model.addAttribute("board", board);
-		//게시물 조회시에 조회수 증가하도록 
+		//게시물 조회시에 조회수 증가하도록 추가
 		boardService.plusViewCnt(idx);
 		return "board/view";
+	}
+	
+	@PostMapping(value = "/board/delete.do")
+	public String deleteBoard(@RequestParam(value = "idx", required = false)Long idx, Model model) {
+		if(idx == null) {
+			// 올바르지 않은 접근 에러 메시지 호출 및 리스트로 리다이렉트(To-Do)
+			return showMessageWithRedirect("올바르지 않은 접근입니다.",
+					"/board/list.do", Method.GET, null, model);
+		}
+		
+		try {
+			boolean isDeleted = boardService.deleteBoard(idx);
+			if(!isDeleted) {
+				//게시글 삭제에 실패했다는 알림 메시지 호출(To-do)
+				return showMessageWithRedirect("게시글 삭제에 실패하였습니다.",
+						"/board/list.do", Method.GET, null, model);
+			}
+		}catch(DataAccessException e) {
+			// 데이터베이스 처리 문제 메시지 호출(To-do)
+			return showMessageWithRedirect("데이터 처리 과정에서 문제가 발생하였습니다.",
+					"/board/list.do", Method.GET, null, model);
+		}catch(Exception e) {
+			// 시스템 오류 메시지 호출(To-do)
+			return showMessageWithRedirect("시스템 문제가 발생하였습니다.",
+					"/board/list.do", Method.GET, null, model);
+		}
+		
+		return showMessageWithRedirect("게시글 삭제를 완료하였습니다.",
+				"/board/list.do", Method.GET, null, model);
+		
 	}
 	
 
